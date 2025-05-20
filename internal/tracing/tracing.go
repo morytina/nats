84f322889813d0fb944fa3b00abd70bb4ea01c12
morytina <jh6434.park@gmail.com>
2025-05-20 20:42:14 +0900

    update

--- internal/tracing/tracing.go
+++ internal/tracing/tracing.go
@@ -2,18 +2,18 @@ package tracing
 
 import (
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/propagation"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
 func InitTracer() error {
-	exporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
-	if err != nil {
-		return err
-	}
+	// exporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
+	// if err != nil {
+	// 	return err
+	// }
 
-	tp := sdktrace.NewTracerProvider(sdktrace.WithBatcher(exporter))
+	//tp := sdktrace.NewTracerProvider(sdktrace.WithBatcher(exporter))
+	tp := sdktrace.NewTracerProvider()
 	otel.SetTracerProvider(tp)
 	otel.SetTextMapPropagator(propagation.TraceContext{})
 
