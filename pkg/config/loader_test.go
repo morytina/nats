90168735f9d45a0fa42d14a23a35ba4f52264449
박재현 <41610124+morytina@users.noreply.github.com>
2025-06-23 22:28:06 +0900

    test: assert config values

--- pkg/config/loader_test.go
+++ pkg/config/loader_test.go
@@ -8,6 +8,11 @@ import (
 
 func TestSampleConfigFile(t *testing.T) {
 	config, err := LoadConfig("../../configs/config.yaml")
-	t.Logf("%+v", config)
 	assert.NoError(t, err)
+	if assert.NotNil(t, config) {
+		assert.Equal(t, "kr-west1", config.Region)
+		assert.Equal(t, "dev2", config.Env)
+		assert.Equal(t, 5, config.Nats.ConnPoolCnt)
+		assert.Equal(t, "localhost:6379", config.Valkey.Addr)
+	}
 }
